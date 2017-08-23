f93ae578b984c3d0b721ca24c0324af5a4169024
Marcos Kubas <marcosarobed@gmail.com>
2017-08-23 17:06:21 -0300

    Functions, Conditionals and Loops

--- stage_2/lesson_2.3_functions/procedures.py
+++ stage_2/lesson_2.3_functions/procedures.py
@@ -12,3 +12,90 @@ def rest_of_string(s):
 print rest_of_string('audacity')
 
 # Add your own code and notes here
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+
+def next_target_page(page):
+	start_link  = page.find('<a href=')
+	start_quote = page.find('"', start_link)
+	end_quote   = page.find('"', start_quote+1)
+	url = page[start_quote+1:end_quote]
+	return url, end_quote
+
+url = next_target_page(page)
+print url
+
+def sum(a, b):
+	a = a + b
+	return a
+
+print sum(1, 5)	
+
+def square(n):
+	return n*n
+
+print square(5)	
+
+def sum3(a, b, c):
+	return a+b+c
+
+print sum3(1,2,3)
+
+def abbaize(a, b):
+	return a+b+b+a
+
+print abbaize('dog', 'cat')	
+
+def find_second(a, b):
+    position = a.find(b)
+    position = a.find(b, position+1)
+    return position
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')	
+
+def bigger(n1, n2):
+	if n2 > n1:
+		return n2
+	else:
+		return n1
+
+print bigger(3, 7)	
+
+print 2 < 3
+
+def is_friend(friend):
+	if friend[:1] == 'D' or friend[:1] == 'N':
+	    return True
+	
+	return False
+
+print is_friend('Diane')
+print is_friend('Fred')
+
+def biggest(a,b,c):
+	big = a
+	if b > big:
+		big = b
+	if c > big:
+		big = c
+	return big
+
+
+print biggest(3, 6, 9)
+#>>> 9
+
+print biggest(6, 9, 3)
+#>>> 9
+
+print biggest(9, 3, 6)
+#>>> 9
+
+print biggest(3, 3, 9)
+#>>> 9
+
+print biggest(9, 3, 9)
+#>>> 9
+
+
+
--- stage_2/lesson_2.4_control_flow_loops/loops.py
+++ stage_2/lesson_2.4_control_flow_loops/loops.py
@@ -15,3 +15,50 @@ def count():
 count()
 
 # Add your own code and notes here
+
+def print_numbers(n):
+	i=0
+	while i < n:
+		i = i + 1
+		print i
+		if i > 2:
+			break
+	return i
+
+print print_numbers(3)
+
+def factorial(n):
+	i      = 0
+	result = 1
+	while i < n:
+		result = result * (n - i)
+		i = i + 1
+	return result
+
+print factorial(6)
+
+def get_next_target(page):
+
+	start_link  = page.find('<a href=')
+	if start_link == -1:
+		return None, 0
+
+	start_quote = page.find('"', start_link)
+	end_quote   = page.find('"', start_quote+1)
+	url = page[start_quote+1:end_quote]
+	return url, end_quote
+
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com"></div><br><br><div class="udacity float-left"><a href="http://google.com"></div>')
+url, end_quote = get_next_target(page)
+
+def print_all_links(page):
+	while page:
+		url, endpos = get_next_target(page)
+		if url:
+			print url
+			page = page[endpos:]
+		else:
+			break
+
+print_all_links(page)
