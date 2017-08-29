decebe7e6d9a3928cb034a2bcf52398dd8eebe1e
Marcos Kubaszewski <marcosarobed@gmail.com>
2017-08-28 22:09:32 -0300

    Loops and Lists

--- stage_2/lesson_2.6_structured_data/for_loops.py
+++ stage_2/lesson_2.6_structured_data/for_loops.py
@@ -14,3 +14,47 @@ myList = [1, 2, [3, 4]]
 print_all_elements(myList)
 
 # Add your own code and notes here
+def measure_udacity(p):
+	count = 0
+	for e in p:
+		if e[0] == 'U':
+			count += 1
+	return count
+
+print measure_udacity(['Katy', 'Marcos'])
+print measure_udacity(['Uncle', 'Ursula'])
+
+def find_element(lista, value):
+	i = 0
+	while i < len(lista):
+		if lista[i] == value:
+			return i
+		i += 1
+	return -1
+
+def find_element_for(lista, value):	
+	i = 0
+	for e in lista:
+		if e == value:
+			return i
+		i += 1
+	return -1
+
+def find_element_index(lista, value):	
+	if value in lista:
+		return lista.index(value)
+	return -1
+
+print find_element([1,2,3], 3)
+print find_element_index([1,2,3], 3)
+print find_element_index([1,2,3], 4)
+
+def union(lista1, lista2):
+	for e in lista2:
+		if e not in lista1:
+			lista1.append(e)
+
+a = [1,2,3]
+b = [2,4,6]
+union(a,b)
+print a 			
